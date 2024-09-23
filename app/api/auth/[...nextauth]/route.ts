e7144167fb1e434cf2eeefcc7a9cdc7355258f5c
Sérgio Mazutti <101757048+Sergiodmj@users.noreply.github.com>
2024-09-23 13:21:27 -0300

    deleted:    app/apps/calendar/page.tsx 	deleted:    app/apps/chat/page.tsx 	deleted:    app/apps/contacts/page.tsx 	deleted:    app/apps/email/compose/page.tsx 	deleted:    app/apps/email/page.tsx 	deleted:    app/apps/email/promotions/page.tsx 	deleted:    app/apps/email/read/page.tsx 	deleted:    app/apps/file-manager/applications/page.tsx 	deleted:    app/apps/file-manager/assets/page.tsx 	deleted:    app/apps/file-manager/documents/page.tsx 	deleted:    app/apps/file-manager/important/page.tsx 	deleted:    app/apps/file-manager/media/page.tsx 	deleted:    app/apps/file-manager/page.tsx 	deleted:    app/apps/file-manager/personal/page.tsx 	deleted:    app/apps/file-manager/projects/page.tsx 	deleted:    app/apps/file-manager/recents/page.tsx 	deleted:    app/apps/kanban-board/page.tsx 	deleted:    app/apps/to-do-list/page.tsx 	new file:   app/libs/auth-config.ts 	deleted:    app/libs/auth.config.ts 	modified:   package.json

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@
 /* eslint-disable no-console */
 /* eslint-disable prettier/prettier */
 import NextAuth from "next-auth"
-import { auth as authOptions } from "app/libs/auth.config";
+import { auth as authOptions } from "app/libs/auth-config";
 
 
 const handler = NextAuth(authOptions)
